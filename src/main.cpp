a2e1a72c2857895fe3701b328e57524f4f3fb652
Maxime Gregot Desktop <gregot.maxime@gmail.com>
2021-03-09 20:51:54 +0100

    Ajout calcul du pas Ajout fonction moveTo Normalement pret pour les premiers tests

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@
 #include "TeensyTimerTool.h"
 using namespace TeensyTimerTool;
 
-#define SPIN_STEP 200
-#define NUM_STEPPER 7
-#define DELAY_HIGH 3.0
+#define SPIN_STEP   200
+#define DELAY_HIGH  3.0
+#define STALL_SPD   1000    // 1 ms
 
+#define PIN_SPIN_0  0
 #define PIN_SPIN_1  1
 #define PIN_SPIN_2  2
 #define PIN_SPIN_3  3
-#define PIN_SPIN_4  4
 
 #pragma endregion
 
@@ -41,7 +41,7 @@ struct stepper
     volatile char           state;              // etat du moteur (a : acceleration ; c : constant ; b : brake)
     volatile long           aim;                // position finale visee par le moteur
 
-    volatile float          speed;              // vitesse (periode) la plus rapide que le moteur va atteindre
+    volatile double         speed;              // vitesse (periode) la plus rapide que le moteur va atteindre
     volatile bool           pinState;           // etat du moteur (HIGH ou LOW)
     volatile char           dir;                // sens de deplacement (+1 vers la droite, -1 vers la gauche)
     bool                    jobDone;            // vrai si le moteur n'a pas a avancer ; faux si le moteur est en train d'avancer/demarrer
@@ -57,8 +57,8 @@ volatile byte stepperFlag;
 volatile double timer;
 volatile unsigned int i;    
 volatile bool canMove;   // vrai si les moteurs sont autorises a bouger
-volatile bool emergency;    // vrai si un moteur touche l'interrupteur de fin de course
-volatile byte emergencyFlag;    // numerote les moteurs qui touchent les interrupteurs de fin de course
+volatile bool emergency = false;    // vrai si un moteur touche l'interrupteur de fin de course
+volatile byte emergencyFlag = 0;    // numerote les moteurs qui touchent les interrupteurs de fin de course
 
 volatile controller controllerList[4];  // liste des commandes
 
@@ -72,7 +72,7 @@ void initStepper(stepper mot)
     mot.pinState = false;
     mot.dir = 1;
     mot.brakeZone = 0;
-    mot.n = 1;
+    mot.n = 0;
     mot.t = (double) 0;
     mot.stepTime = (double) 0;
     mot.jobDone = true;
@@ -95,15 +95,27 @@ void stepAndSetStepTime(volatile unsigned int i)    // fait un pas
         {
             stepperList[i].stepLow;
 
+            if (abs(stepperList[i].motPos - stepperList[i].aim) < stepperList[i].brakeZone) // freinage en cas de depassement ou d'approche de la cible
+            {
+                stepperList[i].state = 'b';
+            }
+
             if (stepperList[i].state == 'a') // le moteur accelere ; calcul de t avec n
             {
+                stepperList[i].n++;
+                if (stepperList[i].n == 1) // calcul de la pause en montee
+                {
+                    stepperList[i].stepTime = stepperList[i].stepTime * (1 - (2 / (double)(4 * stepperList[i].n + 1))) * 0.676;
+                }
+                else
+                {
+                    stepperList[i].stepTime = stepperList[i].stepTime * (1 - (2 / (double)(4 * stepperList[i].n + 1)));
+                }
             }
 
             if (stepperList[i].state == 'c') // le moteur va a la vitesse max, sauf s'il depasse la zone de freinage : 'b' [fait]
             {
-                if (abs(stepperList[i].motPos - stepperList[i].aim) < stepperList[i].brakeZone)
-                    {stepperList[i].state = 'b';}
-                else{stepperList[i].t = stepperList[i].speed - DELAY_HIGH;}
+                stepperList[i].t = stepperList[i].speed - DELAY_HIGH;
             }
 
             if (stepperList[i].state == 'b') // s'arrete si le moteur est arrive sur sa cible ; sinon : calcule le nouveau t
@@ -111,7 +123,22 @@ void stepAndSetStepTime(volatile unsigned int i)    // fait un pas
                 if (stepperList[i].motPos == stepperList[i].aim)
                 {
                     stepperList[i].jobDone = true;
-                    stepperList[i].state = 'a';
+                    stepperList[i].state = 's';
+                }
+                else
+                {
+                    if(stepperList[i].speed < STALL_SPD)
+                    {
+                        stepperList[i].n--;
+                        if (stepperList[i].n == 1) // calcul de la pause en descente
+                        {
+                            stepperList[i].stepTime = stepperList[i].stepTime * (1 + 2 / (double)(4 * stepperList[i].n - 1)) / 0.676;
+                        }
+                        else
+                        {
+                            stepperList[i].stepTime = stepperList[i].stepTime * (1 + 2 / (double)(4 * stepperList[i].n - 1));
+                        }
+                    }
                 }
             }
         }
@@ -122,7 +149,7 @@ void setTimer() // regle le prochain intervalle de pause ; reactualise les flags
 {
     timer = 5000000; // 5000 ms
     stepperFlag = 0;
-    for (i = 0; i < NUM_STEPPER; i++)
+    for (i = 0; i < 7; i++)
     {
         if ((stepperList[i].t <= timer) && (!stepperList[i].jobDone))
         {
@@ -138,7 +165,7 @@ void setTimer() // regle le prochain intervalle de pause ; reactualise les flags
         }
     }
 
-    for (i = 0; i < NUM_STEPPER; i++)
+    for (i = 0; i < 7; i++)
     {
         stepperList[i].t -= timer;
     }
@@ -159,7 +186,7 @@ void timerInterrupt()
     }
     else
     {
-        for(i=0; i<NUM_STEPPER; i++)
+        for(i=0; i<7; i++)
         {
             if(stepperFlag & (1<<i))
             {
@@ -170,7 +197,14 @@ void timerInterrupt()
     }
 }
 
-void spinInterrupt1()   // INTERRUPT COMMANDES
+void spinInterrupt0()   // INTERRUPT COMMANDES
+{
+    if(digitalReadFast(PIN_SPIN_0) == HIGH)                             {controllerList[0].position += SPIN_STEP;}
+    else                                                                {controllerList[0].position -= SPIN_STEP;}
+    if(controllerList[0].position < controllerList[0].lowerBoundary)    {controllerList[0].position = controllerList[0].lowerBoundary;}
+    if(controllerList[0].position > controllerList[0].upperBoundary)    {controllerList[0].position = controllerList[0].upperBoundary;}
+}
+void spinInterrupt1()   
 {
     if(digitalReadFast(PIN_SPIN_1) == HIGH)                             {controllerList[1].position += SPIN_STEP;}
     else                                                                {controllerList[1].position -= SPIN_STEP;}
@@ -193,13 +227,21 @@ void spinInterrupt3()
 
 
 }
-void spinInterrupt4()
+
+
+// INITIATION - ACTUALISATION DU MOUVEMENT
+
+void moveTo(int numStepper, long newAim, double newSpeed)
 {
-    if(digitalReadFast(PIN_SPIN_4) == HIGH)                             {controllerList[4].position += SPIN_STEP;}
-    else                                                                {controllerList[4].position -= SPIN_STEP;}
-    if(controllerList[4].position < controllerList[4].lowerBoundary)    {controllerList[4].position = controllerList[4].lowerBoundary;}
-    if(controllerList[4].position > controllerList[4].upperBoundary)    {controllerList[4].position = controllerList[4].upperBoundary;}
+    stepperList[numStepper].n = 0;
+    stepperList[numStepper].aim = newAim;
+    stepperList[numStepper].speed = newSpeed;
+    if(stepperList[numStepper].aim != stepperList[numStepper].motPos)
+    {
+        stepperList[numStepper].jobDone = false;
+    }
 }
+
 
 
 // FONCTIONS DE SECURITE
@@ -213,14 +255,15 @@ void clearEmgFlags(int stepper_number)
 void setup()
 {
     // COMMANDES
+    pinMode(PIN_SPIN_0, INPUT);
+    attachInterrupt(PIN_SPIN_0, spinInterrupt0, CHANGE);
     pinMode(PIN_SPIN_1, INPUT);
     attachInterrupt(PIN_SPIN_1, spinInterrupt1, CHANGE);
     pinMode(PIN_SPIN_2, INPUT);
     attachInterrupt(PIN_SPIN_2, spinInterrupt2, CHANGE);
     pinMode(PIN_SPIN_3, INPUT);
     attachInterrupt(PIN_SPIN_3, spinInterrupt3, CHANGE);
-    pinMode(PIN_SPIN_4, INPUT);
-    attachInterrupt(PIN_SPIN_4, spinInterrupt4, CHANGE);
+    
 
 
     t1.begin(timerInterrupt);
