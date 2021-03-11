5137d6df07b49521abe03adda88404b866c1f7bc
Maxime Gregot Desktop <gregot.maxime@gmail.com>
2021-03-11 16:17:09 +0100

    Ajout bibliotheques

--- Teensy_implementation/src/main.cpp
+++ Teensy_implementation/src/main.cpp
@@ -1,8 +1,12 @@
 #include <Arduino.h>
+#include <WS2812Serial.h>
+#include "TeensyTimerTool.h"
+
 
 void setup() {
   // put your setup code here, to run once:
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
